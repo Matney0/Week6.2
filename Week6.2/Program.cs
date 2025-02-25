90f8cd71e4c1a55b689be041a59f40df2042dd81
Matney0 <mlmoller21@amarillocollege.com>
2025-02-24 21:17:25 -0600

    Update Program.cs
    Comments

--- Week6.2/Program.cs
+++ Week6.2/Program.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,8 +6,10 @@ using System.Threading.Tasks;
 
 namespace Week6._2
 {
+//Primary class
         public class Product
         {
+//Public properties 
             public string ProductID { get; set; }
             public string ProductName { get; set; }
             public double Price { get; set; }
@@ -19,26 +21,29 @@ namespace Week6._2
                 Price = price;
             }
         }
-
+//class for the shopping cart
         public class ShoppingCart
         {
+//dont want inerfaces and lists to get mixed up. also more private
             private List<Product> products;
 
+//visable shopping cart
             public ShoppingCart()
             {
+//adds products to a list
                 products = new List<Product>();
             }
-
+//Adding products to the Product list
             public void AddProduct(Product product)
             {
                 products.Add(product);
             }
-
+//Removing products from the Product list
             public void RemoveProduct(string productId)
             {
                 products.RemoveAll(p => p.ProductID == productId);
             }
-
+//Method to calculate the price 
             public double CalculateTotalPrice()
             {
                 double total = 0;
@@ -54,30 +59,37 @@ namespace Week6._2
         {
             static void Main(string[] args)
             {
+//Shopping cart list for the shopping cart method
                 ShoppingCart cart = new ShoppingCart();
-
+//Loops while true 
                 while (true)
                 {
+//Menu options for the user to chose from
                     Console.WriteLine("Choose an option:");
                     Console.WriteLine("1. Add Product");
                     Console.WriteLine("2. Remove Product");
                     Console.WriteLine("3. View Total Price");
                     Console.WriteLine("4. Exit");
                     string choice = Console.ReadLine();
-
+//First choice
                     if (choice == "1")
                     {
+//Asks and stores product
                         Console.WriteLine("Enter Product ID:");
                         string productId = Console.ReadLine();
                         Console.WriteLine("Enter Product Name:");
                         string productName = Console.ReadLine();
                         Console.WriteLine("Enter Product Price:");
+//Aks and converts value to computer language 
                         double price = Convert.ToDouble(Console.ReadLine());
 
+//For the product list and all its information/values
                         Product product = new Product(productId, productName, price);
+//Uses AddProdcut method
                         cart.AddProduct(product);
                         Console.WriteLine("Product added to cart.");
                     }
+//Asks for the product name and removes it if found
                     else if (choice == "2")
                     {
                         Console.WriteLine("Enter Product ID to remove:");
@@ -85,14 +97,17 @@ namespace Week6._2
                         cart.RemoveProduct(productId);
                         Console.WriteLine("Product removed from cart.");
                     }
+//Shows the price of all products
                     else if (choice == "3")
                     {
                         Console.WriteLine($"Total Price: ${cart.CalculateTotalPrice():F2}");
                     }
+//Exit
                     else if (choice == "4")
                     {
                         break;
                     }
+//Invalid option entered
                     else
                     {
                         Console.WriteLine("Invalid option. Please try again.");
